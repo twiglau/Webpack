371c470f91fb2b490b108f6ccb2e9b6fb27653de
lau <twigliu@hotmail.com>
2021-06-26 22:45:33 +0800

    proxy

--- 06-使用Dev.Server提高你的本地开发效率.js
+++ 06-使用Dev.Server提高你的本地开发效率.js
@@ -66,5 +66,135 @@
 
 /**
  * 二, Webpack Dev Server
+ * webpack-dev-server 同样也是一个独立的 npm 模块,所以需要通过 npm 将 webpack-dev-server
+ * 作为项目的开发依赖安装. 安装完成后,这个模块为我们提供了一个叫做 webpack-dev-server 的 CLI 
+ * 程序,我们同样可以直接通过 npx 直接去运行这个 CLI, 或者把它定义到 npm scripts 中,如下:
+ * 
+ * > 安装 webpack-dev-server
+ * > npm install webpack-dev-server --save-dev
+ * > 运行 webpack-dev-server
+ * > npx webpack serve
+ * 
+ * 运行webpack-dev-server 这个命令时,它内部会启动一个 HTTP Sever,为打包的结果提供静态文件服务,并且
+ * 自动使用 Webpack 打包我们的应用, 然后监听源代码的变化,一旦文件发生变化,它会立即重新打包.
+ */
+
+/**
+ * 三,配置选项
+ * Webpack配置对象中可以有一个叫做 devServer 的属性,专门用来为 webpack-dev-server 提供配置,如下:
+ * 
+ * // ./webpack.config.js
+ * const path = require('path')
+ * 
+ * module.exports = {
+ *     // ...
+ *     devServer: {
+ *        contentBase: path.join(__dirname,'dist'),
+ *        compress:true,
+ *        port:9000
+ *        //....
+ *        // 详细配置文档 https://webpack.js.org/configuration/dev-server/
+ *     }
+ * }
+ */
+
+/**
+ * 四, 静态资源访问
+ * webpack-dev-server 默认会将构建结果和输出文件全部作为开发服务器的资源文件,也就是说,只要通过 Webpack打包
+ * 能够输出的文件都可以直接被访问到, 但是如果你还有一些没有参与打包的静态文件也需要作为开发服务器的资源被访问,
+ * 那你就需要额外通过配置"告诉" webpack-dev-server.
+ * 
+ * 具体的方法就是在 webpack-dev-server 的配置对象中添加一个对应的配置. 我们回到配置文件中,找到 devServer
+ * 属性,它的类型是一个对象,我们可以通过这个 devServer 对象的 contentBase 属性指定额外的静态资源路径.
+ * 这个contentBase 属性可以是一个字符串或者数组,也就是说你可以配置一个或者多个路径,如下配置:
+ * 
+ * //  ./webpack.config.js
+ * module.exports = {
+ *     // ...
+ *     devServer: {
+ *        contentBase: 'public'
+ *     }
+ * }
+ * 
+ * 我们这里将这个路径设置为项目中的 public 目录,可能有人会有疑问,之前我们在使用插件的时候已经将这个目录通过
+ * copy-webpack-plugin 输出到了输出目录,按照刚刚的说法,所有输出的文件都可以直接被serve,也就是能直接访问
+ * 到,按道理应该不需要作为开发服务器的静态资源路径了
+ * 
+ * 确实是这样的,而且如果你能想到这一点,也就证明你真正理解了webpack-dev-server的文件造价规则.
+ * 
+ * 但是在实际使用Webpack时,我们一般都会把 copy-webpack-plugin 这种插件留在上线前的那一次打包中使用,而开发过程
+ * 中一般不会用它,因为在开发过程中,我们会频繁重复执行打包任务,结社这个目录下需要拷贝的文件比较多,如果每次都需要执行
+ * 这个插件,那打包过程开销就会比较大,每次构建的速度也就自然会降低.
+ * 
+ * 这个我们先移除 CopyWebpackPlugin,确保这里的打包不会输出public目录中的静态资源文件,然后会到命令行再次执行
+ * > npx webpack serve
+ * 启动过后,我们打开浏览器,这里我们访问的页面文件和bundle.js文件均来自于打包结果. 我们再尝试访问 favicon.ico,
+ * 因为这个文件已经没有参与打包了,所以这个文件必然来源于 contentBase中配置的目录了.
+ */
+
+/**
+ * 五,Proxy代理
+ * 由于webpack-dev-server是一个本地开发服务器,所以我们的应用在开发阶段是独立运行在 localhost 的一个端口上,而后端
+ * 服务又是运行在另外一个地址上. 但是最终上线过后,我们的应用一般又会和后端服务部署到同源地址下.
+ * 
+ * 那这样就会出现一个非常常见的问题:
+ * 在实际生产环境中能够直接访问的API,回到我们的开发环境后,再次访问这些API就会产生跨域请求问题.
+ * 
+ * 可能有人会说,我们可以用跨域资源共享(CORS)解决这个问题,确实如此,如果我们请求的后端API支持CORS,那这个问题就不成立了,
+ * 但是并不是每种情况下服务端的API都支持CORS. 如果前后端应用是同源部署,也就是 协议/域名/端口 一致,那这种情况下,根本
+ * 没有必要开启 CORS, 所以跨域请求的问题仍然是不可避免的.
+ * 
+ * 那解决这种开发阶段跨域请求问题最好的办法,就是在开发服务器中配置一个后端API的代理服务,也就是把后端服务代理到本地的开发
+ * 服务地址.
+ * 
+ * webpack-dev-server就支持直接通过配置的方式,添加代理服务. 接下来,来看下具体用法.
+ * 
+ * 这里我们假定Github的API就是我们应用的后端服务,那我们的目标就是将 Github API 代理到本地开发服务器中.
+ * 
+ * Github API 的 Endpoint 都是在跟目录下,也就是说不通的 Endpoint 只是 URL 中的路径部分不通,例如:
+ * https://api.github.com/users 和 https://api.github.com/events
+ * 
+ * 知道API地址的规则过后,我们回到配置文件中,在devServer配置属性中添加一个proxy属性,这个属性需要是一个对象,对象中的每个
+ * 属性就是一个代理规则配置.
+ * 
+ * 属性的名称是需要被代理的请求路径前缀,一般为了辨别,我都会设置为 /api, 值是所对应的代理规则配置,我们将代理目标地址设置Wie:
+ * https://api.github.com, 如下:
+ * 
+ * //   ./webpack.config.js
+ * module.exports = {
+ *    // ...
+ *    devServer: {
+ *       proxy: {
+ *          '/api': {
+ *             target: 'https://api.github.com'
+ *           }
+ *       }
+ *    }
+ * }
+ * 
+ * 那此时我们请求 http://localhost:8080/api/users, 就相当于请求了
+ *              http://api.github.com/api/users
+ * 而我们真正希望请求的地址是 https://api.github.com/uses, 所以对于代理路径开头的 /api 我们要重写掉.我们可以添加一个
+ * pathRewrite 属性来实现代理路径重写,重写规则就是把路径中开头的 /api 替换为空, pathRewrite 最终会以正则的方式来替换
+ * 请求路径.
+ * 
+ * //   ./webpack.config.js
+ * module.exports = {
+ *    // ...
+ *    devServer: {
+ *       proxy: {
+ *         '/api': {
+ *            target: 'https://api.github.com',
+ *            pathRewrite: {
+ *               '^/api': '' // 替换掉dialing地址中的 /api
+ *            }
+ *          }
+ *       }
+ *    }
+ * }
+ * 
+ * 除此之外,我们还需设置一个 changeOrigin 属性为 true. 这是因为默认代理服务器会以我们实际在浏览器中请求的主机名,也就是
+ * localhost:8080 作为代理请求中的主机名. 而一般服务器需要根据请求的主机名判断是哪个网站的请求,那localhost:8080这个主机名,
+ * 对于Github的服务器来说,肯定无法正常请求所以需要修改.
  * 
  */
