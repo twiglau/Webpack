da9dceb6d3effe26b91774a86782eee20e089d70
twiglau <twigliu@hotmail.com>
2022-01-24 15:08:38 +0800

    feat: webpack概念

---
+++ 01-Example/webpack.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const path = require('path')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'production'
+}
