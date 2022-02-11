63a0c34cef3f8543180e417880ca7e8c0d4311c6
twiglau <twigliu@hotmail.com>
2022-02-11 15:01:13 +0800

    feat: 构建版本

---
+++ 07-build-webpack/builder-webpack/lib/webpack.prod.js
@@ -0,0 +1,43 @@
+const cssnano = require('cssnano');
+const merge = require('webpack-merge');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const baseConfig = require('./webpack.base');
+
+const prodConfig = {
+  mode: 'production',
+  plugins: [
+    new OptimizeCSSAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [
+        {
+          module: 'react',
+          entry: 'https://now8.gtimg.com/now/lib/16.8.6/react.min.js',
+          global: 'React',
+        },
+        {
+          module: 'react-dom',
+          entry: 'https://now8.gtimg.com/now/lib/16.8.6/react-dom.min.js',
+          global: 'ReactDOM',
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      minSize: 0,
+      cacheGroups: {
+        commons: {
+          name: 'vendors',
+          chunks: 'all',
+          minChunks: 2,
+        },
+      },
+    },
+  },
+};
+
+module.exports = merge(baseConfig, prodConfig);
